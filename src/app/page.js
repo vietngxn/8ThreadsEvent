71592a663be46266dbd67a609f827453d36f0ed6
Nguyen Toan <nguyentoan.dev@gmail.com>
2026-03-11 13:11:29 +0700

    tao thanh toan

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,12 @@ import Button from "@/components/common/Button/ConfirmButton.jsx";
 import StartingPage from "./startingPage";
 import ConcertEventCard from "@/components/common/Card/ConcertEventCard";
 import ProductOptions from "@/components/common/ProductItem/ProductOptions";
+import CheckoutPage from "@/components/common/Checkout/CheckoutPage";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <ProductOptions />
+      <CheckoutPage />
     </div>
   )
 }
