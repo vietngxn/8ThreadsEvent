5b07a5c90be92097fcffeed9b327804b57709735
TrongDavid614 <trongtan123tan@gmail.com>
2026-03-12 21:04:51 +0700

    add voucher modal component

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import SearchInput from "@/components/common/SearchInput";
-import Image from "next/image";
-import Button from "@/components/common/Button/GoldButton.jsx";
-import StartingPage from "./startingPage";
-import ConcertEventCard from "@/components/Card/ConcertEventCard";
+import VoucherModal from "@/components/common/VoucherModal/VoucherModal";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <CheckoutPage />
+    <div className="flex min-h-screen items-center justify-center bg-[#121212]">
+      <VoucherModal isOpen />
     </div>
-  )
+  );
 }
