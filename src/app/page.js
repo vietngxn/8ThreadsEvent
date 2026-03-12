12273d4466697dd927a35567e652760134ea3532
TrongDavid614 <trongtan123tan@gmail.com>
2026-03-12 19:27:08 +0700

    use page.js from main

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import SearchInput from "@/components/common/SearchInput";
 import Image from "next/image";
 import Button from "@/components/common/Button/GoldButton.jsx";
 import StartingPage from "./startingPage";
@@ -6,8 +7,7 @@ import ConcertEventCard from "@/components/Card/ConcertEventCard";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <StartingPage />
       <CheckoutPage />
     </div>
-  );
+  )
 }
