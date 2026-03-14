be581867e59d95ffd21d9428f7447d2637d692d3
TrongDavid614 <trongtan123tan@gmail.com>
2026-03-15 00:25:58 +0700

    Add BackButton component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import BackButton from "@/components/common/Button/BackButton";
+import GoldButton from "@/components/common/Button/GoldButton";
+import VoucherDetail from "@/components/common/VoucherDetail/VoucherDetail";
 import VoucherModal from "@/components/common/VoucherModal/VoucherModal";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#121212]">
-      <VoucherModal isOpen />
+      <BackButton isOpen />
     </div>
   );
 }
