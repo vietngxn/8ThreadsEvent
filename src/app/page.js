a4ee40c10f929dc8b67be62ab4a9a92516f3b54c
VietNguyxn <viet.nguyenba4605@gmail.com>
2026-03-15 15:13:29 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import VoucherModal from "@/components/common/VoucherModal/VoucherModal";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-[#121212]">
-      <BackButton isOpen />
     </div>
   );
 }
