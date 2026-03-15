e8c84db50d234cd447c424b40504b0a8d4c55fd3
TieuCao86 <caotrung862005@gmail.com>
2026-03-15 15:26:20 +0700

    Refactor ConcertCard component, separate CSS and JSX

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,16 @@ import Image from "next/image";
 import Button from "@/components/common/Button/GoldButton.jsx";
 import StartingPage from "./startingPage";
 import SearchBar from "@/components/common/SearchBar/SearchBar";
+import ProductItem from "@/components/ProductItem/ProductItem";
+import LoginPage from "./login/page";
+import Navbar from "@/components/common/Navbar/Navbar";
+import ConcertCard from "@/components/Card/ConcertCard";
+import ConcertDetail from "@/components/Card/ConCertDetail";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <SearchBar />
+      <ConcertDetail />
     </div>
   );
 }
