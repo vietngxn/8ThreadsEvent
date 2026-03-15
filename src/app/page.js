438f794adfa30b53b9deb5e288ff874529539fa6
VietNguyxn <viet.nguyenba4605@gmail.com>
2026-03-15 15:15:52 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import SearchBar from "@/components/common/SearchBar/SearchBar";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <SearchBar />
     </div>
   );
 }
