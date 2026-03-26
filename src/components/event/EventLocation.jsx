ff3a1a633e450dde11217326937e76662dba80fd
TieuCao86 <caotrung862005@gmail.com>
2026-03-26 21:39:15 +0700

    update hover UI + fix icon + gradient text

--- src/components/event/EventLocation.jsx
+++ src/components/event/EventLocation.jsx
@@ -1,11 +1,14 @@
 import styles from "./EventLocation.module.css";
 import PinIcon from "@/components/common/icons/PinIcon";
 
-export default function EventLocation({ location }) {
+export default function EventLocation({ location, isHover }) {
   return (
     <div className={styles.locationRow}>
-      <PinIcon />
-      <p className={`${styles.locationName} ${styles.gradientText}`}>
+      <PinIcon isHover={isHover} />
+      <p
+        className={`${styles.locationName} ${isHover ? styles.blackText : styles.gradientText
+          }`}
+      >
         {location}
       </p>
     </div>
