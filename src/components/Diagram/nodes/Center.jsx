0bf902d65ec14bb69d150d80f5baf6fb7f95acef
TieuCao86 <caotrung862005@gmail.com>
2026-04-03 00:40:00 +0700

    feat: so do san khau

--- src/components/Diagram/nodes/Center.jsx
+++ src/components/Diagram/nodes/Center.jsx
@@ -1,25 +1,51 @@
+import Node from "./Node";
+
 export default function Center({ active, setActive }) {
+  const gap = 100;
+
   return (
-    <g
-      className={`node ${active === "da-sac" ? "active" : ""}`}
-      onClick={() => setActive("da-sac")}
-    >
-      <rect
-        x="-25"
-        y="-25"
-        width="50"
-        height="50"
-        fill="#00aaff"
+    <g>
+      {/* Node trên */}
+      <g transform={`translate(0 ${-gap / 2})`}>
+        <Node
+          id="da-sac"
+          label="Đa sắc"
+          fill="#00aaff"
+          shape="rect"
+          w={40}
+          h={55}
+          active={active}
+          setActive={setActive}
+        />
+      </g>
+
+      {/* Shape nối */}
+      <path transform="translate(20 3)"
+        d={`
+        M -35 10
+        L 35 10
+        L 35 30
+        L 20 40
+        L -20 40
+        L -35 30
+        Z
+      `}
+        fill="#808080"
       />
 
-      <text
-        textAnchor="middle"
-        dominantBaseline="middle"
-        fill="white"
-        fontWeight="700"
-      >
-        Đa sắc
-      </text>
+      {/* Node dưới */}
+      <g transform={`translate(0 ${gap / 2})`}>
+        <Node
+          id="da-hinh"
+          label="Đa hình"
+          fill="#00aaff"
+          shape="rect"
+          w={40}
+          h={70}
+          active={active}
+          setActive={setActive}
+        />
+      </g>
     </g>
   );
 }
