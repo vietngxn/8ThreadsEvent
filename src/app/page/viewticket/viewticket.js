39d79b6f60f9fa69e91c31cd2eae360dc4328747
trucung <trucung41@gmail.com>
2026-04-21 00:04:40 +0700

    update background viewticketpage

--- src/app/page/viewticket/viewticket.js
+++ src/app/page/viewticket/viewticket.js
@@ -1,28 +1,3 @@
-// "use client";
-// import ConcertEventCard from "@/components/Card/ConcertEventCard";
-// import ConcertDetail from "@/components/Card/ConCertDetail";
-// import BackButton from "@/components/common/Button/BackButton";
-// import styles from "./ViewTicket.module.css";
-
-// export default function ViewTicketPage({ event }) {
-//     return (
-//         <div className={styles.pageContainer}>
-
-//             <div className={styles.navigation}>
-//                 <BackButton label="Trở về" />
-//             </div>
-
-//             <section className={styles.section}>
-//                 <ConcertEventCard data={event} />
-//             </section>
-
-//             <div className={styles.detailSection}>
-//                 <ConcertDetail data={event} />
-//             </div>
-//         </div>
-//     );
-// }
-
 "use client";
 
 import { useRef, useCallback } from "react";
